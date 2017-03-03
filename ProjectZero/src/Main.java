e30f04769593374e7f71c432930b38b8d7ed4f53
heavenlypeach <mubiynacasey@aol.com>
2017-03-02 22:46:57 -0500

    refactor

--- ProjectZero/src/Main.java
+++ ProjectZero/src/Main.java
@@ -1,22 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {		
 	public static void main(String[] args){
-		Calculator calc = new Calculator();
+		String inputValues = "";
+		Calculator c = new Calculator(2);
+		Scanner reader = new Scanner(System.in);
+		int[] integers = new int[c.getNumberOfInputs()];
+		double[] doubles = new double[c.getNumberOfInputs()];
 		
 		System.out.print("Enter two numbers: ");
-		Scanner reader = new Scanner(System.in);
-
-		for(int i = 0; i < calc.getNumberOfInputs(); i++){
+		
+		for(int i = 0; i < c.getNumberOfInputs(); i++){
 			if(reader.hasNextInt()){
-				int num = reader.nextInt();
-				calc.addToIntegerSum(num);	
+				integers[i] = reader.nextInt();
+				inputValues += Integer.toString(integers[i]) + ",";
 			} else if(reader.hasNextDouble()){
-				double num = reader.nextDouble();
-				calc.addToDoubleSum(num);
+				doubles[i] = reader.nextDouble();
+				inputValues += Double.toString(doubles[i]) + ",";
 			}
 		}
-
-		calc.printSum();
+		
+		c.calculateSum(integers, doubles);
+		
+		System.out.print("The sum of ");
+		System.out.print(String.join(" and ", inputValues.split(","))); 
+		System.out.print(" is " + c.getSum());
 	}
 }
